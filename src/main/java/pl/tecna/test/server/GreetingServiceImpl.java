cca4eee2c6a495533321ab548f6c114bd27180e2
bplukasz <bp.lukasz@gmail.com>
2017-05-25 18:15:45 +0200

    Zapis wyrażenia do bazy

--- src/main/java/pl/tecna/test/server/GreetingServiceImpl.java
+++ src/main/java/pl/tecna/test/server/GreetingServiceImpl.java
@@ -4,17 +4,23 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.onami.persist.EntityManagerProvider;
+import org.apache.onami.persist.Transactional;
 import org.codehaus.groovy.control.CompilerConfiguration;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import pl.tecna.test.client.GreetingService;
+import pl.tecna.test.domain.ExpressionEntity;
 import pl.tecna.test.shared.FieldVerifier;
 
 /**
  * The server side implementation of the RPC service.
  */
 public class GreetingServiceImpl implements GreetingService {
+
+	@Inject
+	private EntityManagerProvider em;
 
 	@Inject
 	private HttpSession httpSession;
@@ -42,17 +48,26 @@ public class GreetingServiceImpl implements GreetingService {
 		return "Helloooo, " + input + "!<br><br>I am running " + serverInfo
 				+ ".<br><br>It looks like you are using:<br>" + userAgent;
 	}
-	
+	@Transactional
 	public String saveExpression(String input) throws IllegalArgumentException {
 		input = escapeHtml(input); 
-		httpSession.setAttribute("expression", input);
+		ExpressionEntity expressionEntity =  em.get().find(ExpressionEntity.class, httpSession.getId());
+		if(expressionEntity!=null){
+			expressionEntity.setExpression(input);
+		}
+		else{
+			expressionEntity = new ExpressionEntity();
+			expressionEntity.setSessionId(httpSession.getId());
+			expressionEntity.setExpression(input);
+		}
+		em.get().persist(expressionEntity);
 		return "Expression: "+input+" saved.";
-
 	}
 	
 	public String solveExpression() throws IllegalArgumentException{
-		
-		  String expression = (String)httpSession.getAttribute("expression");
+		  ExpressionEntity expressionEntity =  em.get().find(ExpressionEntity.class, httpSession.getId());
+		  if(expressionEntity==null)return"Expression not found.";
+		  String expression = expressionEntity.getExpression();
 		  Binding binding=new Binding();
 		  binding.setVariable("env",System.getenv());
 		  binding.setVariable("sys",System.getProperties());
